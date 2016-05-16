2ea8378938bc1989fe002e44d43684958c9ef62d
Shinya Ishikawa <ishikawa.s.1027@gmail.com>
2016-05-17 01:09:24 +0900

    updated minesweeper-engine to 0.1.1

--- dest/build.js
+++ dest/build.js
@@ -22259,46 +22259,55 @@ return jQuery;
 }));
 
 },{}],5:[function(require,module,exports){
-const MineSweeper = require('./lib/MineSweeper');
-module.exports = MineSweeper;
+module.exports = require('./lib/MineSweeper');
 
 },{"./lib/MineSweeper":6}],6:[function(require,module,exports){
-(function() {
+'use strict';
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+(function () {
   'use strict';
 
   // CONSTANTS
-  const ROW_NUM = 8;
-  const COL_NUM = 8;
-  const MINE_NUM = 10;
-  const STATUS = {
+
+  var ROW_NUM = 8;
+  var COL_NUM = 8;
+  var MINE_NUM = 10;
+  var STATUS = {
     OK: 'OK',
     ERROR: 'ERROR',
     CABOOM: 'CABOOM'
-  }
+  };
 
   // private property
-  let mineState;
+  var mineState = undefined;
 
   // private function
   function getRandomInt(min, max) {
-    return min + Math.floor(Math.random() * (max - min))
+    return min + Math.floor(Math.random() * (max - min));
   }
 
   function select(arr, num) {
     if (arr.length < num) {
       throw new Error('number of selection exceeds array length');
     }
-    const selectArr = [].concat(arr);
-    const resultArr = [];
-    for (let i = 0; i < num; i++) {
+    var selectArr = [].concat(arr);
+    var resultArr = [];
+    for (var i = 0; i < num; i++) {
       resultArr.push(selectArr.splice(getRandomInt(0, selectArr.length), 1)[0]);
     }
     return resultArr;
   }
 
   //XXX: I/F to be extremely rewritten.
-  class MineSweeper {
-    constructor(rowNum, colNum, mineNum) {
+
+  var MineSweeper = function () {
+    function MineSweeper(rowNum, colNum, mineNum) {
+      _classCallCheck(this, MineSweeper);
+
       if (typeof rowNum === 'undefined') {
         rowNum = ROW_NUM;
       }
@@ -22316,122 +22325,154 @@ module.exports = MineSweeper;
       this.rowNum = rowNum;
       this.colNum = colNum;
       this.mineNum = mineNum;
-    };
-
-    createMineField(rowNum, colNum, mineNum) {
-      if (mineNum >= rowNum * colNum) {
-        throw new Error('too many mines!');
-      }
-
-      const field = [];
-      const shuffleArr = [];
-
-      // create a field
-      for (let i = 0; i < rowNum; i++) {
-        field[i] = [];
-        for (let j = 0; j < colNum; j++) {
-          field[i][j] = null;
-          shuffleArr.push([i, j]);
-        }
-      }
-
-      // fill mines
-      const mineArr = select(shuffleArr, mineNum);
-      for (const val of mineArr) {
-        field[val[0]][val[1]] = -1;
-      }
-
-      return field;
-    };
-
-    createOpenField(rowNum, colNum) {
-      const field = [];
-      for (let i = 0; i < rowNum; i++) {
-        field[i] = [];
-        for (let j = 0; j < colNum; j++) {
-          field[i][j] = false;
-        }
-      }
-
-      return field;
     }
 
-    open(row, col) {
-      if (row < 0 || row >= this.rowNum || col < 0 || col >= this.colNum) {
-        return {
-          status: STATUS.ERROR,
-          message: 'out of field',
-          field: this.getField()
-        };
-      }
-      if (this.openState[row][col]) {
-        return {
-          status: STATUS.ERROR,
-          message: 'already opened',
-          field: this.getField()
-        };
-      }
+    _createClass(MineSweeper, [{
+      key: 'createMineField',
+      value: function createMineField(rowNum, colNum, mineNum) {
+        if (mineNum >= rowNum * colNum) {
+          throw new Error('too many mines!');
+        }
 
-      this.openState[row][col] = true;
-      const ms = this.getMineState(row, col);
-      if (ms === -1) {
-        // TODO: make it Game-Over. Would be better to do it by a wrapper of this class.
-        return {
-          status: STATUS.CABOOM,
-          field: this.openAllField()
-        };
-      } else {
-        if (ms === 0) {
-          for (let i = -1; i < 2; i++) {
-            for (let j = -1; j < 2; j++) {
-              this.open(row + i, col + j);
+        var field = [];
+        var shuffleArr = [];
+
+        // create a field
+        for (var i = 0; i < rowNum; i++) {
+          field[i] = [];
+          for (var j = 0; j < colNum; j++) {
+            field[i][j] = null;
+            shuffleArr.push([i, j]);
+          }
+        }
+
+        // fill mines
+        var mineArr = select(shuffleArr, mineNum);
+        var _iteratorNormalCompletion = true;
+        var _didIteratorError = false;
+        var _iteratorError = undefined;
+
+        try {
+          for (var _iterator = mineArr[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+            var val = _step.value;
+
+            field[val[0]][val[1]] = -1;
+          }
+        } catch (err) {
+          _didIteratorError = true;
+          _iteratorError = err;
+        } finally {
+          try {
+            if (!_iteratorNormalCompletion && _iterator.return) {
+              _iterator.return();
+            }
+          } finally {
+            if (_didIteratorError) {
+              throw _iteratorError;
             }
           }
         }
-        return {
-          status: STATUS.OK,
-          field: this.getField()
-        };
-      }
-    };
 
-    getMineState(row, col) {
-      const state = mineState[row][col];
-      if (state !== null) {
-        return state;
+        return field;
       }
-      let mineNum = 0;
-      for (let i = -1; i < 2; i++) {
-        if (row + i < 0 || row + i >= this.rowNum) {
-          continue;
+    }, {
+      key: 'createOpenField',
+      value: function createOpenField(rowNum, colNum) {
+        var field = [];
+        for (var i = 0; i < rowNum; i++) {
+          field[i] = [];
+          for (var j = 0; j < colNum; j++) {
+            field[i][j] = false;
+          }
         }
-        for (let j = -1; j < 2; j++) {
-          if (col + j < 0 || col + j >= this.colNum) {
+
+        return field;
+      }
+    }, {
+      key: 'open',
+      value: function open(row, col) {
+        if (row < 0 || row >= this.rowNum || col < 0 || col >= this.colNum) {
+          return {
+            status: STATUS.ERROR,
+            message: 'out of field',
+            field: this.getField()
+          };
+        }
+        if (this.openState[row][col]) {
+          return {
+            status: STATUS.ERROR,
+            message: 'already opened',
+            field: this.getField()
+          };
+        }
+
+        this.openState[row][col] = true;
+        var ms = this.getMineState(row, col);
+        if (ms === -1) {
+          // TODO: make it Game-Over. Would be better to do it by a wrapper of this class.
+          return {
+            status: STATUS.CABOOM,
+            field: this.openAllField()
+          };
+        } else {
+          if (ms === 0) {
+            for (var i = -1; i < 2; i++) {
+              for (var j = -1; j < 2; j++) {
+                this.open(row + i, col + j);
+              }
+            }
+          }
+          return {
+            status: STATUS.OK,
+            field: this.getField()
+          };
+        }
+      }
+    }, {
+      key: 'getMineState',
+      value: function getMineState(row, col) {
+        var state = mineState[row][col];
+        if (state !== null) {
+          return state;
+        }
+        var mineNum = 0;
+        for (var i = -1; i < 2; i++) {
+          if (row + i < 0 || row + i >= this.rowNum) {
             continue;
           }
-          mineNum += mineState[row + i][col + j] === -1;
+          for (var j = -1; j < 2; j++) {
+            if (col + j < 0 || col + j >= this.colNum) {
+              continue;
+            }
+            mineNum += mineState[row + i][col + j] === -1;
+          }
         }
+        mineState[row][col] = mineNum;
+        return mineNum;
       }
-      mineState[row][col] = mineNum;
-      return mineNum;
-    }
+    }, {
+      key: 'getField',
+      value: function getField() {
+        return mineState.map(function (arr) {
+          return arr.map(function (val) {
+            return val === -1 ? null : val;
+          });
+        });
+      }
+    }, {
+      key: 'openAllField',
+      value: function openAllField() {
+        return mineState;
+      }
+    }]);
 
-    getField() {
-      return mineState.map((arr) => {
-        return arr.map((val) => {
-          return val === -1 ? null : val
-        })
-      });
-    }
+    return MineSweeper;
+  }();
 
-    openAllField() {
-      return mineState;
-    }
-  };
+  ;
 
   module.exports = MineSweeper;
 })();
-
 },{}],7:[function(require,module,exports){
 'use strict';
 
